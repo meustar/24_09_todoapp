bde63a95d5e7ea724bf803bb382b6dab6effb8c1
meustar <meustar829@gmail.com>
2024-09-30 15:48:19 +0900

    tailwind, mui 충돌 피하는 법

--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,7 @@ import Button from '@mui/material/Button';
 export default function Home() {
   return (
     <>
+      <button>버튼</button>
       <Button className="bg-red-500" variant="contained">
         Contained
       </Button>
