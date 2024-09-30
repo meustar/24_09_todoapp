fbb2aaa8ac911a58aa124d6b1dc75783a049ab80
meustar <meustar829@gmail.com>
2024-09-30 16:44:48 +0900

    MUI 테마 커스터마이징, React Icons

---
+++ src/app/theme.js
@@ -0,0 +1,15 @@
+import { createTheme } from '@mui/material';
+
+const theme = createTheme({
+  palette: {
+    type: 'light',
+    primary: {
+      main: '#f50057',
+    },
+    secondary: {
+      main: '#f50057',
+    },
+  },
+});
+
+export default theme;
