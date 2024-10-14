74ae536729acd8284e19fea38033989b8abb5cc5
meustar <meustar829@gmail.com>
2024-10-14 23:37:46 +0900

    MUI 테마 커스터마이징, React Icons

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,44 @@
 'use client';
+
+import * as React from 'react';
 import Button from '@mui/material/Button';
+import { ThemeProvider } from '@mui/material';
+import { RiDeleteBin5Line } from 'react-icons/ri';
+import theme from './theme';
 
 export default function Home() {
   return (
     <>
-      <button>버튼</button>
-      <Button className="tw-bg-red-500" variant="contained">
-        Contained
-      </Button>
+      <ThemeProvider theme={theme}>
+        <div className="tw-flex tw-items-center tw-gap-x-3">
+          <Button variant="text">Text</Button>
+          <Button
+            variant="contained"
+            onClick={() => {
+              confirm('삭제 하겠습니까?');
+            }}>
+            <RiDeleteBin5Line />
+            삭제
+          </Button>
+          <Button variant="outlined">Outlined</Button>
+        </div>
+      </ThemeProvider>
+
+      <div className="tw-flex tw-items-center tw-gap-x-3 tw-mt-3">
+        <Button
+          variant="text"
+          onClick={() => {
+            alert('버튼 클릭 됨');
+          }}>
+          Text
+        </Button>
+        <Button variant="contained" disabled>
+          Contained
+        </Button>
+        <Button variant="outlined" href="sub/">
+          sub 로 이동
+        </Button>
+      </div>
     </>
   );
 }
