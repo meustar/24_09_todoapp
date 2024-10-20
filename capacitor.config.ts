d6ecf4819b2d3d07893339d2944c1ec7a52a49b1
meustar <meustar829@gmail.com>
2024-10-21 03:23:53 +0900

    안드로이트 스튜디오에서 에뮬로 todoapp 실행 완료

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,10 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.example.app',
   appName: 'todoapp',
-  webDir: 'public'
+  webDir: 'out',
+  server: {
+    androidScheme: 'https',
+  },
 };
 
 export default config;
