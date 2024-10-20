5762133d3e12836ff0345c4794f6f34163ee332e
meustar <meustar829@gmail.com>
2024-10-20 20:16:12 +0900

    리스트 역순 출력

--- src/app/page.js
+++ src/app/page.js
@@ -28,7 +28,7 @@ function useTodoStatus() {
       content: newContent,
       regDate: dateToStr(new Date()),
     };
-    setTodos((todos) => [...todos, newTodo]);
+    setTodos((todos) => [newTodo, ...todos]);
   };
 
   const removeTodo = (id) => {
@@ -157,7 +157,7 @@ function App() {
   React.useEffect(() => {
     todosState.addTodo('스쿼트');
     todosState.addTodo('벤치프레스');
-    todosState.addTodo('데드리프트');
+    todosState.addTodo('데드리프트\n런지');
   }, []);
   const onSubmit = (e) => {
     e.preventDefault();
